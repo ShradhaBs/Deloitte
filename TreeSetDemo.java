96b6410179094b96d518f9ec8d810a3aea757569
ShradhaBs <59157473+ShradhaBs@users.noreply.github.com>
2019-12-31 16:27:18 +0530

    Add files via upload

---
+++ TreeSetDemo.java
@@ -0,0 +1,14 @@
+package javaproj2;
+
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+      public static void main(String[] args) {
+    	  	TreeSet names=new TreeSet();
+    	  	names.add("ga");
+    	  	names.add("ash");
+    	  	names.add("za");
+    	  	System.out.println(names);
+    	  	
+      }
+}
